e5e70af53afa00d28930c46e693d88b1b428bf68
kshitijashetty <kshitijashetty100@gmail.com>
2019-07-23 09:17:04 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const path = require('path');
+const bodyParser = require('body-parser')
+const router = express.Router();
+
+
+const app = express() 
+const port = 5000 
+
+
+app.use(express.static(__dirname + '/'));
+app.set('views', __dirname + "/views");
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  userName = req.query.name;
+  userId = req.query.id;
+  res.render("index", {userName: userName, userId: userId});
+})	
+
+// app.post('/', (req, res) => {
+  // console.log(req.body)
+  
+  // res.send({
+    // replies: [{
+      // type: 'text',
+      // content: att1,
+    // }], 
+    // conversation: {
+      // memory: { key: 'value' }
+    // }
+  // })
+// })
+
+// app.post('/errors', (req, res) => {
+  // console.log(req.body) 
+  // res.send() 
+// }) 
+
+app.listen(port, () => { 
+  console.log('Server is running on port 5000') 
+})
